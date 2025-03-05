2215f36d0bd3b951d522a651e8c6ba902692c59c
Vishwansh Kumar <kumarvishwansh@gmail.com>
2025-03-05 23:16:20 +0530

    test pay 3

--- src/app/api/payments/phonepe/route.ts
+++ src/app/api/payments/phonepe/route.ts
@@ -1,43 +1,18 @@
-// import { randomUUID } from "crypto";
-// import crypto from "crypto";
-// import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
-// import axios from "axios";
-// import { client } from "@/lib/phonepe";
-// import { StandardCheckoutPayRequest } from "pg-sdk-node";
-// import axios from "axios";
 
 let cachedToken: string | null = null;
-let merchantId: string | null = null;
 
 export async function POST(req: NextRequest) {
   try {
     const request = await req.json();
     console.log(request.udf1);
-    // console.log("SAFDA", process.env.PHONEPE_CLIENT_ID);
-    // const MERCHANT_KEY = "96434309-7796-489d-8924-ab56988a6076";
-    // const MERCHANT_ID = "PGTESTPAYUAT86";
-    // const MERCHANT_BASE_URL =
-    //   "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay";
+
     const urlEncodedData = new URLSearchParams({
       client_id: `${process.env.PHONEPE_CLIENT_ID}`,
-      client_version: `${1}`, // Convert to string dynamically
+      client_version: `${1}`,
       client_secret: `${process.env.PHONEPE_API_KEY}`,
       grant_type: "client_credentials",
     }).toString();
-    // const tokenUrl =
-    //   "https://api-preprod.phonepe.com/apis/pg-sandbox/v1/oauth/token";
-    // const tokenOptions = {
-    //   method: "POST",
-    //   url: tokenUrl,
-    //   headers: {
-    //     // accept: "application/json",
-    //     "Content-Type": "application/x-www-form-urlencoded",
-    //   },
-    //   data: {
-    //     body: urlEncodedData,
-    //   },
-    // };
     const tokenResponse = await fetch(
       "	https://api.phonepe.com/apis/identity-manager/v1/oauth/token",
       {
@@ -55,29 +30,13 @@ export async function POST(req: NextRequest) {
     const tokenObj = await tokenResponse.json();
     cachedToken = tokenObj.access_token;
     const prod_URL = "https://api.phonepe.com/apis/pg/checkout/v2/pay";
-    // const orderId = "T" + Date.now();
     if (
       !process.env.NEXT_PUBLIC_DOMAIN_NAME ||
       !process.env.PHONEPE_MERCHANT_ID
     ) {
-      // console.log("KEMKfmK");
       return NextResponse.json({ error: "No env" });
     }
-    // const redirectUrl = `${process.env.NEXT_PUBLIC_DOMAIN_NAME}/api/payments/status`;
-    // const successUrl = "http://localhost:3000/payment/success";
-    // const failureUrl = "http://localhost:3000/payment/failure";
-    // const paymentPayload = {
-    //   merchantId: process.env.PHONEPE_MERCHANT_ID,
-    //   merchantUserId: `${request.name}`,
-    //   mobileNumber: `${request.phone}`,
-    //   amount: Number(request.amount) * 100,
-    //   merchantTransactionId: orderId,
-    //   redirectUrl: `${redirectUrl}/?id=${orderId}`,
-    //   redirectMode: "POST",
-    //   paymentInstrument: {
-    //     type: "PAY_PAGE",
-    //   },
-    // };
+    const merchantOrderId = "T" + Date.now();
     const response = await fetch(`${prod_URL}`, {
       method: "POST",
       headers: {
@@ -85,7 +44,7 @@ export async function POST(req: NextRequest) {
         Authorization: `O-Bearer ${cachedToken}`,
       },
       body: JSON.stringify({
-        merchantOrderId: "T" + Date.now(),
+        merchantOrderId: merchantOrderId,
         amount: 100,
         expireAfter: 1200,
         metaInfo: {
@@ -99,7 +58,7 @@ export async function POST(req: NextRequest) {
           type: "PG_CHECKOUT",
           message: "Payment message used for collect requests",
           merchantUrls: {
-            redirectUrl: `${process.env.NEXT_PUBLIC_DOMAIN_NAME}/api/payments/phonepe`,
+            redirectUrl: `${process.env.NEXT_PUBLIC_DOMAIN_NAME}/api/payments/status/?merchantId=${merchantOrderId}`,
           },
         },
       }),
@@ -109,70 +68,19 @@ export async function POST(req: NextRequest) {
       return NextResponse.json({ error: "Initialization error" });
     }
     const initialtePayment = await response.json();
-    merchantId = initialtePayment.merchantOrderId;
+    if (!initialtePayment) {
+      console.log("SereE");
+      return NextResponse.json({ error: "Initialization error" });
+    }
     console.log(initialtePayment);
-    // console.log("res", response.data.data.instrumentResponse.redirectInfo.url);
     return NextResponse.json({
       message: `${initialtePayment.redirectUrl}`,
     });
-    // return NextResponse.json({
-    //   message: `${"GG"}`,
-    // });
   } catch (err) {
     console.log(err);
     return NextResponse.json(
       { error: "Internal Server Error" },
       { status: 500 }
     );
-  }
-  // const { amount, redirectUrl } = await req.json();
-
-  // const merchantOrderId = randomUUID();
-
-  // const payRequest = StandardCheckoutPayRequest.builder()
-  //   .merchantOrderId(merchantOrderId)
-  //   .amount(amount)
-  //   .redirectUrl(redirectUrl)
-  //   .build();
-  // try {
-  //   const response = await client.pay(payRequest);
-  //   return NextResponse.json({ checkoutPageUrl: response.redirectUrl });
-  // } catch (err) {
-  //   console.log(err);
-  //   return NextResponse.json(
-  //     { error: "Payment initiation failed" },
-  //     { status: 500 }
-  //   );
-  // }
-}
-
-export async function GET(req: NextRequest) {
-  try {
-    const request = await req.json();
-    console.log(request);
-    const orderStatusResponse = await fetch(
-      `https://api.phonepe.com/apis/pg/checkout/v2/order/${merchantId}/status?details=false&errorContext=true`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `O-Bearer ${cachedToken}`,
-        },
-      }
-    );
-    if (!orderStatusResponse) {
-      return NextResponse.json({ error: "Order Status Error" });
-    }
-    const orderStatusObj = await orderStatusResponse.json();
-    if (orderStatusObj.state !== "COMPLETED") {
-      return NextResponse.redirect(
-        new URL("/payment/failure", process.env.NEXT_PUBLIC_DOMAIN_NAME)
-      );
-    }
-    return NextResponse.redirect(
-      new URL("/payment/success", process.env.NEXT_PUBLIC_DOMAIN_NAME)
-    );
-  } catch (err) {
-    console.log(err);
   }
 }
---
+++ src/app/api/payments/status/route.ts
@@ -0,0 +1,60 @@
+import { NextRequest, NextResponse } from "next/server";
+
+let cachedToken: string | null = null;
+
+export async function GET(req: NextRequest) {
+  try {
+    const searchParams = req.nextUrl.searchParams;
+    const merchantId = searchParams.get("merchantId");
+    // const request = await req.json();
+    // console.log(request.udf1);
+    const urlEncodedData = new URLSearchParams({
+      client_id: `${process.env.PHONEPE_CLIENT_ID}`,
+      client_version: `${1}`,
+      client_secret: `${process.env.PHONEPE_API_KEY}`,
+      grant_type: "client_credentials",
+    }).toString();
+    const tokenResponse = await fetch(
+      "	https://api.phonepe.com/apis/identity-manager/v1/oauth/token",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: urlEncodedData,
+      }
+    );
+    if (!tokenResponse) {
+      console.log("SJE");
+      return NextResponse.json({ error: "No token" });
+    }
+    const tokenObj = await tokenResponse.json();
+    cachedToken = tokenObj.access_token;
+    // const request = await req.json();
+    // console.log(request);
+    const orderStatusResponse = await fetch(
+      `https://api.phonepe.com/apis/pg/checkout/v2/order/${merchantId}/status?details=false&errorContext=true`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `O-Bearer ${cachedToken}`,
+        },
+      }
+    );
+    if (!orderStatusResponse) {
+      return NextResponse.json({ error: "Order Status Error" });
+    }
+    const orderStatusObj = await orderStatusResponse.json();
+    if (orderStatusObj.state !== "COMPLETED") {
+      return NextResponse.redirect(
+        new URL("/payment/failure", process.env.NEXT_PUBLIC_DOMAIN_NAME)
+      );
+    }
+    return NextResponse.redirect(
+      new URL("/payment/success", process.env.NEXT_PUBLIC_DOMAIN_NAME)
+    );
+  } catch (err) {
+    console.log(err);
+  }
+}
