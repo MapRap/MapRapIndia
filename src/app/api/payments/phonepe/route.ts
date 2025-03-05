fe7df0e4be133e11ffc024d7cbd2066f131c803d
Vishwansh Kumar <kumarvishwansh@gmail.com>
2025-03-05 22:28:31 +0530

    test pay 2

--- src/app/api/payments/phonepe/route.ts
+++ src/app/api/payments/phonepe/route.ts
@@ -99,7 +99,7 @@ export async function POST(req: NextRequest) {
           type: "PG_CHECKOUT",
           message: "Payment message used for collect requests",
           merchantUrls: {
-            redirectUrl: "http://localhost:3000/api/payments/phonepe",
+            redirectUrl: `${process.env.NEXT_PUBLIC_DOMAIN_NAME}/api/payments/phonepe`,
           },
         },
       }),
