743f7a3c7c0efae717bc72bcd87d5a5f5934830c
MapRap <maprapindia@gmail.com>
2025-03-18 00:23:41 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,9 +9,6 @@ import {
   publicRoutes,
 } from "../route";
 
-
-//     return NextResponse.redirect(new URL("/unauthorized", request.url));
-//   } else if (
 //     pathname.startsWith("/admin/addPlan") &&
 //     !["owner"].includes(`${payload.type}`)
 //   ) {
